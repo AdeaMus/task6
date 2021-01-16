0aa2d98dec28a68a87ec8c32006348c900d20470
Alex <Shiko_89@mail.ru>
2021-01-16 22:54:02 +0300

    Auto stash before merge of "master" and "origin/master"

--- task62.py
+++ task62.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 class Road:
     def __init__(self, _length, _width):
@@ -17,3 +18,24 @@ class MassCount(Road):
 r = MassCount(25, 10000, 125)
 print(r.mass())
 
+=======
+
+class Road:
+    def __init__(self, _length, _width):
+        self._length = _length
+        self._width = _width
+
+    def mass(self):
+        return self._length * self._width
+
+
+class MassCount(Road):
+    def __init__(self, _length, _width, volume):
+        super().__init__(_length, _width)
+        self.volume = volume
+
+
+r = MassCount(25, 10000, 125)
+print(r.mass())
+
+>>>>>>> Stashed changes
--- task65.py
+++ task65.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 class Stationary:
     def __init__(self, title):
         self.title = title
@@ -35,4 +36,43 @@ pencil = Pencil('Карандаш')
 handle = Handle('Маркер')
 print(pen.draw())
 print(pencil.draw())
+=======
+class Stationary:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+class Pen(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки ручкой'
+
+
+class Pencil(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки карандашом'
+
+
+class Handle(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки маркером'
+
+
+pen = Pen('Ручка')
+pencil = Pencil('Карандаш')
+handle = Handle('Маркер')
+print(pen.draw())
+print(pencil.draw())
+>>>>>>> Stashed changes
 print(handle.draw())
